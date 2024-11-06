5fe44b63b183461cba9e5f5e15d3b5c7d3b27ae0
m-slekys <125832115+m-slekys@users.noreply.github.com>
2024-11-06 02:26:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,53 +34,55 @@ main = duckdb.read_csv(path)
 id = int(st.text_input("Enter ID", value = 121007))
 
 test = duckdb.sql(f'SELECT COUNT (SK_ID_CURR) FROM main WHERE SK_ID_CURR = {id}').fetchall()[0][0]
-st.write(test)
+if test == 0:
+    st.write("NO SUCH ID IN TABLE)
+else:
 
-# Get row from main table
-querry = "SELECT * FROM main WHERE SK_ID_CURR =" + str(id)
-
-
-main = duckdb.sql(querry).fetchdf()
-
-# Read CSV files as DuckDB relations.
-path = "csv/bureau.csv"
-bureau_csv = duckdb.read_csv(path)
-path = "csv/credit_card_balance.csv"
-credit_card_balance_csv = duckdb.read_csv(path)
-path = "csv/installments_payments.csv"
-installments_payments_csv = duckdb.read_csv(path)
-path = "csv/POS_CASH_balance.csv"
-POS_CASH_balance_csv = duckdb.read_csv(path)
-path = "csv/previous_application.csv"
-previous_application_csv = duckdb.read_csv(path)
-
-querry = f"WITH bureau AS ({bureau_query}), \
-credit_card_balance AS ({credit_card_balance_query}), \
-installments_payments AS ({installments_payments_query}), \
-POS_CASH_balance AS ({POS_CASH_balance_query}), \
-previous_application AS ({previous_application_query}) \
-SELECT * FROM main AS t LEFT JOIN bureau AS b on t.SK_ID_CURR = b.SK_ID_CURR \
-LEFT JOIN credit_card_balance AS c on t.SK_ID_CURR = c.SK_ID_CURR \
-LEFT JOIN installments_payments AS i on t.SK_ID_CURR = i.SK_ID_CURR \
-LEFT JOIN POS_CASH_balance AS p on t.SK_ID_CURR = p.SK_ID_CURR \
-LEFT JOIN previous_application AS pa on t.SK_ID_CURR = pa.SK_ID_CURR \
-\
-;"
-
-df = duckdb.sql(querry).fetchdf()
-st.write(df)
-df.drop(['TARGET', 'column000', 'column000_1'], inplace=True, axis = 1)
-df = pd.DataFrame(preprocessor.transform(df))
-
-
-
-df_2 = df.join (np.log10(df), rsuffix='log')
-df_2.replace(to_replace = np.log10(0), value = 2.2250738585072014e-308, inplace = 
-True)
-df_2.fillna(0, inplace=True)
-
-pred = (XGB_reg.predict(df) + log_reg.predict(df_2))/2
-adjusted_pred = [round(i,4) for i in make_it_linear.predict(pred)]
-
-output_text = 'Predicted probability of default: ' + str(adjusted_pred[0]*100) + '%'
-st.write(output_text)
+    # Get row from main table
+    querry = "SELECT * FROM main WHERE SK_ID_CURR =" + str(id)
+    
+    
+    main = duckdb.sql(querry).fetchdf()
+    
+    # Read CSV files as DuckDB relations.
+    path = "csv/bureau.csv"
+    bureau_csv = duckdb.read_csv(path)
+    path = "csv/credit_card_balance.csv"
+    credit_card_balance_csv = duckdb.read_csv(path)
+    path = "csv/installments_payments.csv"
+    installments_payments_csv = duckdb.read_csv(path)
+    path = "csv/POS_CASH_balance.csv"
+    POS_CASH_balance_csv = duckdb.read_csv(path)
+    path = "csv/previous_application.csv"
+    previous_application_csv = duckdb.read_csv(path)
+    
+    querry = f"WITH bureau AS ({bureau_query}), \
+    credit_card_balance AS ({credit_card_balance_query}), \
+    installments_payments AS ({installments_payments_query}), \
+    POS_CASH_balance AS ({POS_CASH_balance_query}), \
+    previous_application AS ({previous_application_query}) \
+    SELECT * FROM main AS t LEFT JOIN bureau AS b on t.SK_ID_CURR = b.SK_ID_CURR \
+    LEFT JOIN credit_card_balance AS c on t.SK_ID_CURR = c.SK_ID_CURR \
+    LEFT JOIN installments_payments AS i on t.SK_ID_CURR = i.SK_ID_CURR \
+    LEFT JOIN POS_CASH_balance AS p on t.SK_ID_CURR = p.SK_ID_CURR \
+    LEFT JOIN previous_application AS pa on t.SK_ID_CURR = pa.SK_ID_CURR \
+    \
+    ;"
+    
+    df = duckdb.sql(querry).fetchdf()
+    st.write(df)
+    df.drop(['TARGET', 'column000', 'column000_1'], inplace=True, axis = 1)
+    df = pd.DataFrame(preprocessor.transform(df))
+    
+    
+    
+    df_2 = df.join (np.log10(df), rsuffix='log')
+    df_2.replace(to_replace = np.log10(0), value = 2.2250738585072014e-308, inplace = 
+    True)
+    df_2.fillna(0, inplace=True)
+    
+    pred = (XGB_reg.predict(df) + log_reg.predict(df_2))/2
+    adjusted_pred = [round(i,4) for i in make_it_linear.predict(pred)]
+    
+    output_text = 'Predicted probability of default: ' + str(adjusted_pred[0]*100) + '%'
+    st.write(output_text)
